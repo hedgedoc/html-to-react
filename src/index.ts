00dc1f0b79b73333293e0dc96055d93c4e3969b4
Youri Noel Nelson <ynnelson@users.noreply.github.com>
2022-03-28 16:01:04 -0400

    Exposing some of the other functions in Index file

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,10 @@
 
 import { convertHtmlToReact } from './convertHtmlToReact'
 export default convertHtmlToReact
+
+export { default as processNodes } from './processNodes';
+export { default as convertNodeToReactElement } from './convertNodeToReactElement';
+export { default as NodeToReactElementTransformer } from './NodeToReactElementTransformer';
+
+// expose htmlparser2 so it can be used if required
+export { default as htmlparser2 } from 'htmlparser2';

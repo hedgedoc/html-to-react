1e9c4f3ff93756b50e8be43146c6527a4e2439a8
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-06-06 11:49:30 +0200

    Re-Export common functions
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import * as htmlparser2 from 'htmlparser2'
 
 export { convertHtmlToReact as default } from './convertHtmlToReact'
 export { convertNodeToReactElement } from './convertNodeToReactElement'
+export type { NodeToReactElementTransformer } from './NodeToReactElementTransformer'
+export { processNodes } from './processNodes'
+export { htmlparser2 }

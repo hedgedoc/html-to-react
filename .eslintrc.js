4058b7e2e0d3745687c33a68b97490152e807403
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-07-01 23:23:00 +0200

    Add prettier to eslint
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 /*
- * SPDX-FileCopyrightText: 2020 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
  *
- * SPDX-License-Identifier: CC0-1.0
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: [
-      "./tsconfig.json"
-    ]
+    project: ['./tsconfig.json']
   },
-  plugins: [
-    "@typescript-eslint"
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ]
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
 }

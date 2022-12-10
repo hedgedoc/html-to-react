cc4ee58cb222bd78d98d7c3077b003448306de35
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-12-10 10:45:37 +0100

    fix: remove export of transitive libs
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import * as htmlparser2 from 'htmlparser2'
-
 export { convertHtmlToReact as default, ParserOptions } from './convertHtmlToReact'
 export { convertNodeToReactElement } from './convertNodeToReactElement'
 export type { NodeToReactElementTransformer } from './NodeToReactElementTransformer'
 export { processNodes } from './processNodes'
-export { htmlparser2 }
